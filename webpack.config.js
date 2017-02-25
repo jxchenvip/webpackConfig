bbdb4de2f59343759085eecc3ac84400fdac15a7
xiaochen <928272182@qq.com>
2017-02-25 20:38:47 +0800

    'update'

--- webpack.config.js
+++ webpack.config.js
@@ -16,8 +16,9 @@ var ncp = require("copy-paste"); // 复制，粘贴
 var rootdir = path.join(__dirname, 'src');
 var childProcess = require('child_process');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var minify = require('html-minifier').minify;
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 var pkg = require('./package.json');
-var alias = pkg.spm.alias; // 别名
 var cwd = process.cwd();
 // 处理参数
 var argvs = process.argv.reduce(function(o, key, index, arr) {
@@ -92,7 +93,8 @@ var sls = {
             if (fs.lstatSync(fileName).isDirectory() && sPath != '') {
                 if (deep) readdir(fileName, deep);
             } else {
-                if (!/(^(_|grunt|gulp|webpack)|\.map$)/.test(sPath) && /\.js$/.test(sPath)) {
+                // if (!/(^(_|grunt|gulp|webpack)|\.map$)/.test(sPath) && /\.js$/.test(sPath)) {
+                if (!/(^(_|grunt|gulp|webpack)|\.map$)/.test(sPath)) {
                     var name = path.relative(cwd, fileName);
                     files[name.replace(/\.js$/, '')] = fileName;
                 }
@@ -100,7 +102,7 @@ var sls = {
         })
     };
     readdir(cwd, sls.deep);
-    console.log(files)
+    console.log(files);
     sls.entry = files;
 })();
 
@@ -121,6 +123,8 @@ module.exports = {
                 loader: 'babel-loader',
                 query: { presets: ['es2015'] }
             },
+            { test: /\.jade$/, loader: 'jade?pretty=true' },
+            // { test: /\.html$/, loader: "html" },
             //.css 文件使用 style-loader 和 css-loader 来处理
             { test: /\.styl$/, loader: ExtractTextPlugin.extract(["css", "autoprefixer-loader?browsers=last 9 version", "stylus"]) },
             { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader") },
@@ -141,9 +145,9 @@ module.exports = {
         //查找module的话从这里开始查找
         root: rootdir,
         //自动扩展文件后缀名，意味着我们require模块可以省略不写后缀名
-        extensions: ['', '.js', '.json', '.scss', '.styl'],
+        extensions: ['', '.js', '.json', '.scss', '.styl', '.jade'],
         //模块别名定义，方便后续直接引用别名，无须多写长长的地址
-        alias: alias
+        alias: {}
     },
     plugins: [
         new webpack.optimize.UglifyJsPlugin({
@@ -157,3 +161,28 @@ module.exports = {
         new ExtractTextPlugin("css/style.css")
     ]
 }
+
+// 打包jade页面
+Object.keys(module.exports.entry).forEach(function(page) {
+    if (page !== 'vendors') {
+        module.exports.plugins.push(new HtmlWebpackPlugin({
+            // title: 用来生成页面的 title 元素
+            // filename: 输出的 HTML 文件名，默认是 index.html, 也可以直接配置带有子目录。
+            // template: 模板文件路径，支持加载器，比如 html!./index.html
+            // inject: true | 'head' | 'body' | false  ,注入所有的资源到特定的 template 或者 templateContent 中，如果设置为 true 或者 body，所有的 javascript 资源将被放置到 body 元素的底部，'head' 将放置到 head 元素中。
+            // favicon: 添加特定的 favicon 路径到输出的 HTML 文件中。
+            // minify: {} | false , 传递 html-minifier 选项给 minify 输出
+            // hash: true | false, 如果为 true, 将添加一个唯一的 webpack 编译 hash 到所有包含的脚本和 CSS 文件，对于解除 cache 很有用。
+            // cache: true | false，如果为 true, 这是默认值，仅仅在文件修改之后才会发布文件。
+            // showErrors: true | false, 如果为 true, 这是默认值，错误信息会写入到 HTML 页面中
+            // chunks: 允许只添加某些块 (比如，仅仅 unit test 块)
+            // chunksSortMode: 允许控制块在添加到页面之前的排序方式，支持的值：'none' | 'default' | {function}-default:'auto'
+            // excludeChunks: 允许跳过某些块，(比如，跳过单元测试的块) 
+            title: page,
+            filename: page + '.html',
+            minify: false,
+            template: './app/src/jade/' + page + '.jade',
+            chunks: [page]
+        }));
+    }
+})
