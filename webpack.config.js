cfcb6a0e21c1bc5e4aae888719551381944023e9
xiaochen <928272182@qq.com>
2017-02-25 13:51:01 +0800

    'update'

--- webpack.config.js
+++ webpack.config.js
@@ -127,12 +127,12 @@ module.exports = {
             //.scss 文件使用 style-loader、css-loader 和 sass-loader 来编译处理
             { test: /\.scss$/, loader: 'style!css!sass?sourceMap' },
             //图片文件使用 url-loader 来处理，小于8kb的直接转为base64
-            { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' }
+            { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192&name=./images/[name].[ext]' }
         ]
     },
-    stylusOther: {
-        use: [autoprefixer]
-    },
+    // stylusOther: {
+    //     use: [autoprefixer]
+    // },
     externals: {
         // jquery: 'jQuery',
         zepto: '$',
